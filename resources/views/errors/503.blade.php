34b91bbc7e3d205eacc207b1f996d63926bb3a9b
Tek Xylo <tekxilog@gmail.com>
2020-10-10 16:35:58 -0300

    Mainteance Message

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+
+                <div class="card-body">
+
+                        <center>
+                        <br>
+                        <img class="mb-3" src="impact-w.png" alt="" width="272">
+                        <br><br>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
