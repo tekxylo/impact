31060d4a6fea62ad1a769aa2e2fbb12973cc600c
Tek Xylo <tekxilog@gmail.com>
2020-10-10 16:40:41 -0300

    Update Notice

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -11,8 +11,11 @@
                         <center>
                         <br>
                         <img class="mb-3" src="impact-w.png" alt="" width="272">
-                        <br><br>
-
+                        <br><hr>
+                        <h2 style="margin-top:5px;" class="text-danger"><i class="fad fa-cogs"></i></h2>
+                  <p style="padding:0px;margin:0px;"><strong>Update Notice</strong></p>
+                  <p>Impact is currently being updated. Try again later!</p>
+                  <i class="fas fa-spinner fa-spin"></i> Updating Livewire Components...
                 </div>
             </div>
         </div>
